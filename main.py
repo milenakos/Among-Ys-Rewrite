da196eb9681d004f6548708f09e55ac9f43dd23a
milena-kos <75163549+milena-kos@users.noreply.github.com>
2021-09-11 18:16:38 +0300

    Code refactor

--- main.py
+++ main.py
@@ -6,32 +6,6 @@ import random, math, os, pygame, requests, json
 pygame.init()
 pygame.font.init()
 
-do_kill = False
-do_write = False
-kill_possible = True
-ticks = 0
-FPS = 120
-x = -2800
-y = -450
-change = 12
-orient = "Left"
-names = ["Xiaoness191", "Milenakos", "SOKE®", "RubiK", "=/", "^ Frinkifail ^", "m(._.)m",
-         "Andrew Meep", "atomicgrape908", "suffix.", "CatRBLX", "Deltaonetrooper", "drealy", "Drewskibob",
-         "Euro2555", "nclazrfpyt", "finched also poppi alpha", "FloppityLopp", "GentleToonMan", "GentleToonMan",
-         "J'zargo", "lucyy", "m(._.)mV2", "Mr. snowmanfan20_YT", "normal", "exotic", "Stoned", "That_One_Amogus_Person",
-         "ThisIsMyUsername", "tiddcunt", "Vana_Ungkap", "WarNoodleZ", "Wut", "Yuuta Togashi", "Zatrex", "FedeGamer", "hezigonic"]
-colors = ["Red", "Blue", "Green", "Pink", "Orange", "Yellow", "Black", "White", "Purple", "Brown", "Cyan", "Lime", "Maroon", "Rose", "Banana", "Gray",
-          "Tan", "Coral", "Olive", "Fortegreen"]
-moves = []
-bots = []
-
-screen = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption("Among Ys Rewrite Beta")
-clock = pygame.time.Clock()
-all_sprites = pygame.sprite.Group()
-
-back = pygame.image.load('img/loadings/loading.png')
-
 class Name(pygame.sprite.Sprite):
     def __init__(self, name):
         self.name = name
@@ -45,7 +19,7 @@ class Name(pygame.sprite.Sprite):
         self.image.set_colorkey((0,0,0))
         self.rect = self.image.get_rect()
 
-    def update(self, where):
+    def update(self, where, screen):
         self.rect.center = where
         screen.blit(self.image, self.rect)
 
@@ -53,7 +27,7 @@ class Name(pygame.sprite.Sprite):
         return self.name
 
 class Crew(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, colors):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("img/crew/" + random.choice(colors) + ".png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (74, 99))
@@ -62,15 +36,15 @@ class Crew(pygame.sprite.Sprite):
 
         self.name = Name("Player")
 
-    def update(self):
-        self.name.update(self.rect.center)
+    def update(self, x, y, screen, orient):
+        self.name.update(self.rect.center, screen)
         if orient == "Right":
             screen.blit(self.image, self.rect)
         elif orient == "Left":
             screen.blit(pygame.transform.flip(self.image, True, False), self.rect)
 
 class Bot(pygame.sprite.Sprite):
-    def __init__(self, idd):
+    def __init__(self, idd, colors, names, ticks):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("img/crew/" + random.choice(colors) + ".png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (74, 99))
@@ -82,11 +56,11 @@ class Bot(pygame.sprite.Sprite):
         names.pop(self.future_name)
         self.ticks = ticks
 
-    def update(self):
+    def update(self, x, y, screen, orient):
         self.ticks += 1
         try:
             self.rect.center = (x - self.moves[self.ticks][0] + 640, y - self.moves[self.ticks][1] + 360)
-            self.name.update(self.rect.center)
+            self.name.update(self.rect.center, screen)
             if self.moves[self.ticks][2] == "Right":
                 screen.blit(self.image, self.rect)
             else:
@@ -100,123 +74,179 @@ class Bot(pygame.sprite.Sprite):
     def get_coords(self):
         return self.moves[self.ticks][0], self.moves[self.ticks][1]
 
+def main():
+    do_kill = False
+    do_write = False
+    do_super_write = False
+    kill_possible = True
+    ticks = 0
+    FPS = 120
+    x = -2800
+    y = -450
+    change = 12
+    orient = "Left"
+    names = ["Xiaoness191", "Milenakos", "SOKE®", "RubiK", "=/", "^ Frinkifail ^", "m(._.)m",
+             "Andrew Meep", "atomicgrape908", "suffix.", "CatRBLX", "Deltaonetrooper", "drealy", "Drewskibob",
+             "Euro2555", "nclazrfpyt", "finched also poppi alpha", "FloppityLopp", "GentleToonMan", "GentleToonMan",
+             "J'zargo", "lucyy", "m(._.)mV2", "Mr. snowmanfan20_YT", "normal", "exotic", "Stoned", "That_One_Amogus_Person",
+             "ThisIsMyUsername", "tiddcunt", "Vana_Ungkap", "WarNoodleZ", "Wut", "Yuuta Togashi", "Zatrex", "FedeGamer", "hezigonic"]
+    colors = ["Red", "Blue", "Green", "Pink", "Orange", "Yellow", "Black", "White", "Purple", "Brown", "Cyan", "Lime", "Maroon", "Rose", "Banana", "Gray",
+              "Tan", "Coral", "Olive", "Fortegreen"]
+    moves = []
+    bots = []
 
-walls = pygame.image.load("img/layout.png")
-walls_mask = pygame.mask.from_surface(walls)
+    screen = pygame.display.set_mode((1280, 720))
+    pygame.display.set_caption("Among Ys Rewrite Beta")
+    clock = pygame.time.Clock()
+    all_sprites = pygame.sprite.Group()
 
-hitbox = pygame.image.load("img/hitbox.png")
-hitbox_mask = pygame.mask.from_surface(hitbox)
+    back = pygame.image.load('img/loadings/loading.png')
 
-kill = pygame.image.load("img/kill.png")
-kill_btn = kill.get_rect()
-kill_btn.center = (1200, 640)
+    walls = pygame.image.load("img/layout.png")
+    walls_mask = pygame.mask.from_surface(walls)
 
-screen.blit(back, (0, 0))
+    hitbox = pygame.image.load("img/hitbox.png")
+    hitbox_mask = pygame.mask.from_surface(hitbox)
 
-running = True
+    kill = pygame.image.load("img/kill.png")
+    kill_btn = kill.get_rect()
+    kill_btn.center = (1200, 640)
 
-while running:
-    clock.tick(FPS)
-    ticks += 1
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1 and kill_btn.collidepoint(event.pos):
-                do_kill = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                do_kill = True
+    screen.blit(back, (0, 0))
 
-    if do_kill and kill_possible:
-        dists = []
-        for i in bots:
-            dists.append(i.distance_from_center())
-        if min(dists) <= 270:
-            enemy_ind = dists.index(min(dists))
-            enemy = bots[enemy_ind]
-            x, y = enemy.get_coords()
-            bots.pop(enemy_ind)
+    running = True
+
+    while running:
+        clock.tick(FPS)
+        ticks += 1
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1 and kill_btn.collidepoint(event.pos):
+                    do_kill = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    do_kill = True
+
+        if do_kill and kill_possible:
+            dists = []
+            for i in bots:
+                dists.append(i.distance_from_center())
+            if min(dists) <= 270:
+                enemy_ind = dists.index(min(dists))
+                enemy = bots[enemy_ind]
+                x, y = enemy.get_coords()
+                bots.pop(enemy_ind)
+                textSurf = font.render("People left: " + str(len(bots) + 1), 1, (255, 255, 255))
+                image = pygame.Surface((1280, 720))
+                image.blit(textSurf, [0, 0])
+                image.set_colorkey((0,0,0))
+            do_kill = False
+            if len(bots) == 0:
+                kill_possible = False
+
+        keys = pygame.key.get_pressed()
+
+        x_save, y_save = x, y
+           
+        new_x = new_y = 0
+
+        if do_super_write:
+            if random.randint(0, 15) == 0 or do_super_write == True:
+                if random.randint(0, 1) == 0:
+                    if random.randint(0, 1) == 0:
+                        new_x += change
+                        do_super_write = 4
+                    else:
+                        new_x -= change
+                        do_super_write = 1
+                else:
+                    if random.randint(0, 1) == 0:
+                        new_y += change
+                        do_super_write = 2
+                    else:
+                        new_y -= change
+                        do_super_write = 3
+            else:
+                if do_super_write == 4:
+                    new_x += change
+                elif do_super_write == 1:
+                    new_x -= change
+                elif do_super_write == 2:
+                    new_y += change
+                elif do_super_write == 3:
+                    new_y -= change
+
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            new_x += change
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            new_x -= change
+
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            new_y += change
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            new_y -= change
+
+        if new_x > 0:
+            orient = "Left"
+        elif new_x < 0:
+            orient = "Right"
+
+        x += new_x
+        y += new_y
+
+        if do_write:
+            moves.append([x, y, orient])
+
+        if walls_mask.overlap(hitbox_mask, (-x, -y)):
+            x, y = x_save, y_save
+
+        if ticks == 2:
+            latest = requests.get("https://api.github.com/repos/milena-kos/Among-Ys-Rewrite/releases/latest").text
+            version = json.loads(latest)["name"]
+            if version != v:
+                font = pygame.font.Font("arlrdbd.ttf", 30)
+                new_ver = font.render("New version of Among Ys Rewrite is available. Please upgrade your game.", 1, (255, 255, 255))
+            back = pygame.image.load('img/skeld.png')
+            crew = Crew(colors)
+            if not do_write:
+                for i in range(0, len(os.listdir(".\\moves"))):
+                    bot = Bot(i, colors, names, ticks)
+                    bots.append(bot)
+            all_sprites.add(crew)
+
+            font = pygame.font.Font("arlrdbd.ttf", 35)
             textSurf = font.render("People left: " + str(len(bots) + 1), 1, (255, 255, 255))
             image = pygame.Surface((1280, 720))
             image.blit(textSurf, [0, 0])
             image.set_colorkey((0,0,0))
-        do_kill = False
-        if len(bots) == 0:
-            kill_possible = False
 
-    keys = pygame.key.get_pressed()
-
-    x_save, y_save = x, y
-       
-    new_x = new_y = 0
-
-    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-        new_x += change
-    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-        new_x -= change
-
-    if keys[pygame.K_w] or keys[pygame.K_UP]:
-        new_y += change
-    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-        new_y -= change
-
-    if new_x > 0:
-        orient = "Left"
-    elif new_x < 0:
-        orient = "Right"
-
-    x += new_x
-    y += new_y
+        screen.fill((0, 0, 0))
+        
+        if ticks > 1:
+            screen.blit(back, (x, y))
+            for i in bots:
+                i.update(x, y, screen, orient)
+            screen.blit(image, (0, 0))
+            if kill_possible:
+                screen.blit(kill, kill_btn)
+            walls.get_rect().center = (x, y)
+            if new_ver:
+                screen.blit(new_ver, (0, 680))
+        else:
+            screen.blit(back, (0, 0))
+        
+        all_sprites.update(x, y, screen, orient)
+        pygame.display.flip()
 
     if do_write:
-        moves.append([x, y, orient])
+        file = open("moves\\file.txt", "w")
+        file.write(str(moves))
+        file.close()
 
-    if walls_mask.overlap(hitbox_mask, (-x, -y)):
-        x, y = x_save, y_save
+    pygame.quit()
 
-    if ticks == 2:
-        latest = requests.get("https://api.github.com/repos/milena-kos/Among-Ys-Rewrite/releases/latest").text
-        version = json.loads(latest)["name"]
-        if version != v:
-            font = pygame.font.Font("arlrdbd.ttf", 30)
-            new_ver = font.render("New version of Among Ys Rewrite is available. Please upgrade your game.", 1, (255, 255, 255))
-        back = pygame.image.load('img/skeld.png')
-        crew = Crew()
-        if not do_write:
-            for i in range(0, len(os.listdir(".\\moves"))):
-                bot = Bot(i)
-                bots.append(bot)
-        all_sprites.add(crew)
-
-        font = pygame.font.Font("arlrdbd.ttf", 35)
-        textSurf = font.render("People left: " + str(len(bots) + 1), 1, (255, 255, 255))
-        image = pygame.Surface((1280, 720))
-        image.blit(textSurf, [0, 0])
-        image.set_colorkey((0,0,0))
-
-    screen.fill((0, 0, 0))
-    
-    if ticks > 1:
-        screen.blit(back, (x, y))
-        for i in bots:
-            i.update()
-        screen.blit(image, (0, 0))
-        if kill_possible:
-            screen.blit(kill, kill_btn)
-        walls.get_rect().center = (x, y)
-        if new_ver:
-            screen.blit(new_ver, (0, 680))
-    else:
-        screen.blit(back, (0, 0))
-    
-    all_sprites.update()
-    pygame.display.flip()
-
-if do_write:
-    file = open("moves\\file.txt", "w")
-    file.write(str(moves))
-    file.close()
-
-pygame.quit()
+if __name__ == "__main__":
+    main()
