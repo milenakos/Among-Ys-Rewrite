d82270be7509a1a816df9bbc7998cfe9dd5035a8
milena-kos <75163549+milena-kos@users.noreply.github.com>
2021-11-03 19:39:44 +0300

    fix typo in server.py license

--- server.py
+++ server.py
@@ -1,18 +1,10 @@
 # License for this file:
 #
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#                    Version 2, December 2004
+#            DO WHAT THE HECK YOU WANT TO PUBLIC LICENSE
 #
-# Copyright (C) 2021  Milenakos
-#
-# Everyone is permitted to copy and distribute verbatim or modified
-# copies of this license document, and changing it is allowed as long
-# as the name is changed.
-#
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
 #   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 #
-#  0. You just DO WHAT THE FUCK YOU WANT TO.
+#  0. You just DO WHAT THE HECK YOU WANT TO.
 
 
 import socket
