6fb13fb78a8f408e6b392fded973af6b543bee86
milena-kos <75163549+milena-kos@users.noreply.github.com>
2021-11-27 14:05:38 +0300

    add app icon to build (not sure if it works)

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
-pyinstaller -w -F --add-data "img;img" --add-data "moves;moves" --add-data "arlrdbd.ttf;." main.py
+pyinstaller -w -F -i="img/icon.png" --add-data "img;img" --add-data "moves;moves" --add-data "arlrdbd.ttf;." --main.py
 move dist\main.exe .
 ren "main.exe" "AmongYsRewrite.exe"
