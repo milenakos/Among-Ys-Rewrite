28984fc5cc40cdb1c5d5b46fb7794defc13e1e01
milena-kos <75163549+milena-kos@users.noreply.github.com>
2022-04-01 20:06:18 +0300

    revert 32 bits build

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
-%localappdata%/Programs/Python/Python38-32/python.exe -m pyinstaller -w -F -i="img/icon.ico" --add-data "img;img" --add-data "moves;moves" --add-data "arlrdbd.ttf;." main.py
+pyinstaller -w -F -i="img/icon.ico" --add-data "img;img" --add-data "moves;moves" --add-data "arlrdbd.ttf;." main.py
 move dist\main.exe .
 ren "main.exe" "AmongYsRewrite.exe"
